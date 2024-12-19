d8996b3c5615e17fab4ce79ed1b714bf4ca6ed61
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-12-19 23:44:50 +0400

    Added frontend tests to the workflow.

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+global.clearImmediate = (fn) => {
+    clearTimeout(fn); // Заменяем clearImmediate на clearTimeout
+  };

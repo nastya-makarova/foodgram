ac4b55bf06d53f0708b6f1beabd5bd6e8ed66c41
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-12-22 23:11:05 +0400

    Testing another path for redirect.

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -44,7 +44,7 @@ User = get_user_model()
 def redirect_to_recipe(request, short_link):
     try:
         short_link = ShortLinkRecipe.objects.get(short_link=short_link)
-        return redirect(f'/api/recipes/{short_link.recipe.id}')
+        return redirect(f'/recipes/{short_link.recipe.id}')
     except ShortLinkRecipe.DoesNotExist:
         return redirect('/')
 

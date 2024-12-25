623e7d937669eb1ff780d3066111cb5a7f79cd79
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-12-25 22:26:29 +0400

    Fixed the base path and the path in the load data file.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production

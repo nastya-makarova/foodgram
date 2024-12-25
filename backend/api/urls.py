789f913d6b39a801e6b9984c3a9079751361a6f5
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-12-26 00:15:21 +0400

    Added action method to download shopping cart; deleted extra urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,7 @@ from .views import (
     RecipeViewSet,
     TagViewSet,
     FoodgramUserViewSet,
-    APIDownloadShoppingList,
+    # APIDownloadShoppingList,
     APIShoppingList,
     APIListSubscriptions,
     APISubscription,
@@ -21,10 +21,13 @@ router_v1 = DefaultRouter()
 
 router_v1.register('tags', TagViewSet)
 router_v1.register('recipes', RecipeViewSet)
-router_v1.register('ingredients', IngredientViewSet)
+router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('recipes/download_shopping_cart/', APIDownloadShoppingList.as_view()),
+    # path(
+    # 'recipes/download_shopping_cart/',
+    # APIDownloadShoppingList.as_view()
+    # ),
     path('recipes/<int:pk>/shopping_cart/', APIShoppingList.as_view()),
     path('recipes/<int:pk>/favorite/', APIFavorite.as_view()),
     path('users/subscriptions/', APIListSubscriptions.as_view()),
